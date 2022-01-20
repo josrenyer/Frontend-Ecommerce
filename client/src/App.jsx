4200537553d1eff6bb69b5656fcbcb819d7d520b
Silvio Marquez <s.marquez@hotmail.com>
2022-01-20 12:53:37 -0300

    instalacion bootstrap

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import {Route, Routes} from "react-router-dom";
 import Landing from "./pages/Landing";
-import NotFound from "./pages/NotFound"
+import NotFound from "./pages/NotFound";
+import Home from "./pages/Home"
 
 function App() {
 
@@ -8,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-      <Route exact path="/" element={<Landing/>}/>
+      <Route exact path="/" element={<Home/>}/>
       <Route exact path="*" element={<NotFound/>}/>
     </Routes>
     </div>
